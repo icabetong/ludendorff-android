d64efd526f78caf7b303bd415acb1f90dee8e7a3
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-07-23 15:20:52 +0800

    TwoLineSwitch is now TwoLineCheckBox

--- app/src/main/java/io/capstone/keeper/components/views/TwoLineCheckBox.kt
+++ app/src/main/java/io/capstone/keeper/components/views/TwoLineCheckBox.kt
@@ -6,15 +6,15 @@ import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.text.style.TextAppearanceSpan
 import android.util.AttributeSet
-import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.core.content.ContextCompat
 import io.capstone.keeper.R
 
-class TwoLineSwitch @JvmOverloads constructor(
+class TwoLineCheckBox @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.switchStyle
-): SwitchCompat(context, attributeSet, defStyleAttr) {
+    defStyleAttr: Int = R.attr.checkboxStyle
+): AppCompatCheckBox(context, attributeSet, defStyleAttr) {
 
     private val titleSpan: TextAppearanceSpan
     private val subtitleSpan: TextAppearanceSpan
@@ -46,27 +46,27 @@ class TwoLineSwitch @JvmOverloads constructor(
         }
 
     init {
-        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.TwoLineSwitch,
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.TwoLineCheckBox,
             defStyleAttr, 0)
 
         try {
-            title = typedArray.getString(R.styleable.TwoLineSwitch_titleText) ?: ""
-            subtitle = typedArray.getString(R.styleable.TwoLineSwitch_subtitleText)
+            title = typedArray.getString(R.styleable.TwoLineCheckBox_titleText) ?: ""
+            subtitle = typedArray.getString(R.styleable.TwoLineCheckBox_subtitleText)
 
-            titleTextColor = typedArray.getColor(R.styleable.TwoLineSwitch_titleTextColor,
+            titleTextColor = typedArray.getColor(R.styleable.TwoLineCheckBox_titleTextColor,
                 titleTextColor)
-            subtitleTextColor = typedArray.getColor(R.styleable.TwoLineSwitch_subtitleTextColor,
+            subtitleTextColor = typedArray.getColor(R.styleable.TwoLineCheckBox_subtitleTextColor,
                 subtitleTextColor)
 
             val titleTextAppearance = typedArray.getResourceId(
-                R.styleable.TwoLineSwitch_titleTextAppearance,
+                R.styleable.TwoLineCheckBox_titleTextAppearance,
                 R.style.TextAppearance_Keeper_Switch
             )
             titleSpan = TextAppearanceSpan(context, titleTextAppearance)
             titleTextColorSpan = ForegroundColorSpan(titleTextColor)
 
             val subtitleTextAppearance = typedArray.getResourceId(
-                R.styleable.TwoLineSwitch_subtitleTextAppearance,
+                R.styleable.TwoLineCheckBox_subtitleTextAppearance,
                 R.style.TextAppearance_Keeper_Caption
             )
             subtitleSpan = TextAppearanceSpan(context, subtitleTextAppearance)
